012271e2803e70ae1b0ce012a7b1049a329371d2
vinayak <trivedivinayak57@gmail.com>
2024-01-10 06:28:43 +0530

    integration test for resizing

--- src/__tests__/Integration/navbar.spec.tsx
+++ src/__tests__/Integration/navbar.spec.tsx
@@ -1,3 +1,25 @@
-test('test', () => {
-  expect(true).toBeTruthy()
-})
+import { render, act } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Navbar from '../../components/Navbar';
+
+describe('Navbar', () => {
+  it('switches between desktop and mobile versions on window resize', () => {
+    const { getByTestId } = render(<Navbar />);
+    const navbarDesktop = getByTestId('navbar_desktop');
+
+    act(() => {
+      window.innerWidth = 1200;
+      window.dispatchEvent(new Event('resize'));
+    });
+
+    expect(navbarDesktop).toBeInTheDocument();
+
+    act(() => {
+      window.innerWidth = 600;
+      window.dispatchEvent(new Event('resize'));
+    });
+    const navbarMobile = getByTestId('navbar_mobile');
+
+    expect(navbarMobile).toBeInTheDocument();
+  });
+});
